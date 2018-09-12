adf784c73c8fb957a150f17ff15b26acb030e2b8
Your Name <you@example.com>
2018-09-12 10:17:50 +0200

    Authorization added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -39,4 +41,29 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+        # Ensure correct user.
+        #def correct_user
+        #  unless current_user.id == params[:id]
+        #    flash[:danger] = "You can not edit an other users profile!"
+        #    redirect_to root_path
+        #  end
+        #  
+        #end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless correct_user? @user
+    end
 end
